851c69f0a0dfd6aee34eefb1bad4f953bb2e7f05
solielpai <37949408+solielpai@users.noreply.github.com>
2019-05-12 21:31:03 +0800

    Update lonlat.py

--- lonlat.py
+++ lonlat.py
@@ -1,40 +1,245 @@
-  
-import pickle     
-import xlrd  
-import numpy as np
-    #import chardet  
-data = xlrd.open_workbook("tude.xls")  
-table = data.sheets()[0]  
-nrows = table.nrows  
-dict={}
-a=0
-value=[0,0]
-for i in range(nrows):  
-    
-    #print chardet.detect(str(table.row_values(i)))  
-    #print (str(table.row_values(i)).encode("utf-8").decode("utf-8"))
-    key=str(table.row_values(i)[0]+table.row_values(i)[1]+table.row_values(i)[2])
-    print(key)   #province city and county
-    value[0]=float(table.row_values(i)[3])
-    value[1]=float(table.row_values(i)[4])
-    print(value)
-    dict[key]=value[0:2]
-   
-   
-    
-lon=float(input('please input longitude :'))
-lat=float(input('please input  latitude:'))
-dist={}
+#task1
 
-def find_city(a,b):
-    e=[10000,1]
-    for key in dict.keys():
-        dist[key]=np.sqrt((a-dict[key][0])*(a-dict[key][0])+(b-dict[key][1])*(b-dict[key][1]))
-       
-       #print(dist)
-        if float(e[0])>dist[key]:
-            e[0]=dist[key]
-            e[1]=key
-    print(a)
-    return e
-print('the driver is at:',find_city(lon,lat))
+idbm数据集下载及load
+import keras
+import numpy as np
+import tensorflow as tf
+imdb = keras.datasets.imdb
+#下载数据imbd  训练集及标签，测试集及标签
+(x_t, y_t), (x_te, y_te) = imdb.load_data(num_words=10000)
+#这里
+print(x_t.shape,y_t.shape,x_te.shape)
+
+print(len(x_t[0]),len(x_t[1]))#第一二条影评的词向量长度：218,189不相同
+
+#将词向量转化为文本
+word_index = imdb.get_word_index()
+word_index = {k:(v+3) for k, v in word_index.items()}
+word_index['<PAD>'] = 0
+word_index['<START>'] = 1
+word_index['<UNK>'] = 2
+word_index['<UNUSED>'] = 3
+
+reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
+#使所有数组的长度相同
+x_t= keras.preprocessing.sequence.pad_sequences(x_t,
+                                                        value=word_index["<PAD>"],
+                                                        padding='post',
+                                                        maxlen=256)
+x_te = keras.preprocessing.sequence.pad_sequences(x_te,
+                                                       value=word_index["<PAD>"],
+                                                       padding='post',
+                                                       maxlen=256)
+#构造模型
+vocab_size = 10000
+
+model = keras.Sequential()
+model.add(keras.layers.Embedding(vocab_size, 16))
+model.add(keras.layers.GlobalAveragePooling1D())
+model.add(keras.layers.Dense(16, activation=tf.nn.relu))
+model.add(keras.layers.Dense(1, activation=tf.nn.sigmoid))
+
+model.summary()
+model.compile(optimizer=tf.train.AdamOptimizer(),
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+x_val =x_t [:10000]
+partial_x_train = x_t[10000:]
+
+y_val = y_t[:10000]
+partial_y_train = y_t[10000:]
+
+history = model.fit(partial_x_train,
+                    partial_y_train,
+                    epochs=40,
+                    batch_size=512,
+                    validation_data=(x_val, y_val),
+                    verbose=1)
+#评估模型
+results = model.evaluate(x_te, y_te)
+#损失和准确率
+print(results)
+#task2
+#下载THUCNews数据集，四个：train，valid和test，vocab
+# coding: utf-8
+
+import sys
+from collections import Counter
+
+import numpy as np
+import tensorflow.contrib.keras as kr
+import tensorflow as tf
+def native_content(content):
+
+
+    return content
+
+
+def open_file(filename, mode='r'):
+    """
+    常用文件操作，可在python2和python3间切换.
+    mode: 'r' or 'w' for read or write
+    """
+
+    return open(filename, mode, encoding='utf-8', errors='ignore')
+
+
+
+def read_file(filename):
+    """读取文件数据"""
+    contents, labels = [], []
+    with open_file(filename) as f:
+        for line in f:
+            try:
+                label, content = line.strip().split('\t')
+                if content:
+                    contents.append(list(native_content(content)))
+                    labels.append(native_content(label))
+            except:
+                pass
+    return contents, labels
+
+
+def build_vocab(train_dir, vocab_dir, vocab_size=5000):
+    """根据训练集构建词汇表，存储"""
+    data_train, _ = read_file(train_dir)
+
+    all_data = []
+    for content in data_train:
+        all_data.extend(content)
+
+    counter = Counter(all_data)
+    count_pairs = counter.most_common(vocab_size - 1)
+    words, _ = list(zip(*count_pairs))
+    # 添加一个 <PAD> 来将所有文本pad为同一长度
+    words = ['<PAD>'] + list(words)
+    open_file(vocab_dir, mode='w').write('\n'.join(words) + '\n')
+
+
+def read_vocab(vocab_dir):
+    """读取词汇表"""
+    # words = open_file(vocab_dir).read().strip().split('\n')
+    with open_file(vocab_dir) as fp:
+        # 如果是py2 则每个值都转化为unicode
+        words = [native_content(_.strip()) for _ in fp.readlines()]
+    word_to_id = dict(zip(words, range(len(words))))
+    return words, word_to_id
+
+
+def read_category():
+    """读取分类目录，固定"""
+    categories = ['体育', '财经', '房产', '家居', '教育', '科技', '时尚', '时政', '游戏', '娱乐']
+
+    categories = [native_content(x) for x in categories]
+
+    cat_to_id = dict(zip(categories, range(len(categories))))
+
+    return categories, cat_to_id
+
+
+def to_words(content, words):
+    """将id表示的内容转换为文字"""
+    return ''.join(words[x] for x in content)
+
+
+def process_file(filename, word_to_id, cat_to_id, max_length=600):
+    """将文件转换为id表示"""
+    contents, labels = read_file(filename)
+
+    data_id, label_id = [], []
+    for i in range(len(contents)):
+        data_id.append([word_to_id[x] for x in contents[i] if x in word_to_id])
+        label_id.append(cat_to_id[labels[i]])
+
+    # 使用keras提供的pad_sequences来将文本pad为固定长度
+    x_pad = kr.preprocessing.sequence.pad_sequences(data_id, max_length)
+    y_pad = kr.utils.to_categorical(label_id, num_classes=len(cat_to_id))  # 将标签转换为one-hot表示
+
+    return x_pad, y_pad
+
+
+def batch_iter(x, y, batch_size=64):
+    """生成批次数据"""
+    data_len = len(x)
+    num_batch = int((data_len - 1) / batch_size) + 1
+
+    indices = np.random.permutation(np.arange(data_len))
+    x_shuffle = x[indices]
+    y_shuffle = y[indices]
+
+    for i in range(num_batch):
+        start_id = i * batch_size
+        end_id = min((i + 1) * batch_size, data_len)
+        yield x_shuffle[start_id:end_id], y_shuffle[start_id:end_id]
+
+
+
+class TCNNConfig(object):
+    """CNN配置参数"""
+
+    embedding_dim = 64  # 词向量维度
+    seq_length = 600  # 序列长度
+    num_classes = 10  # 类别数
+    num_filters = 256  # 卷积核数目
+    kernel_size = 5  # 卷积核尺寸
+    vocab_size = 5000  # 词汇表达小
+
+    hidden_dim = 128  # 全连接层神经元
+
+    dropout_keep_prob = 0.5  # dropout保留比例
+    learning_rate = 1e-3  # 学习率
+
+    batch_size = 64  # 每批训练大小
+    num_epochs = 10  # 总迭代轮次
+
+    print_per_batch = 100  # 每多少轮输出一次结果
+    save_per_batch = 10  # 每多少轮存入tensorboard
+
+
+class TextCNN(object):
+    """文本分类，CNN模型"""
+
+    def __init__(self, config):
+        self.config = config
+
+        # 三个待输入的数据
+        self.input_x = tf.placeholder(tf.int32, [None, self.config.seq_length], name='input_x')
+        self.input_y = tf.placeholder(tf.float32, [None, self.config.num_classes], name='input_y')
+        self.keep_prob = tf.placeholder(tf.float32, name='keep_prob')
+
+        self.cnn()
+
+    def cnn(self):
+        """CNN模型"""
+        # 词向量映射
+        with tf.device('/cpu:0'):
+            embedding = tf.get_variable('embedding', [self.config.vocab_size, self.config.embedding_dim])
+            embedding_inputs = tf.nn.embedding_lookup(embedding, self.input_x)
+
+        with tf.name_scope("cnn"):
+            # CNN layer
+            conv = tf.layers.conv1d(embedding_inputs, self.config.num_filters, self.config.kernel_size, name='conv')
+            # global max pooling layer
+            gmp = tf.reduce_max(conv, reduction_indices=[1], name='gmp')
+
+        with tf.name_scope("score"):
+            # 全连接层，后面接dropout以及relu激活
+            fc = tf.layers.dense(gmp, self.config.hidden_dim, name='fc1')
+            fc = tf.contrib.layers.dropout(fc, self.keep_prob)
+            fc = tf.nn.relu(fc)
+
+            # 分类器
+            self.logits = tf.layers.dense(fc, self.config.num_classes, name='fc2')
+            self.y_pred_cls = tf.argmax(tf.nn.softmax(self.logits), 1)  # 预测类别
+
+        with tf.name_scope("optimize"):
+            # 损失函数，交叉熵
+            cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=self.logits, labels=self.input_y)
+            self.loss = tf.reduce_mean(cross_entropy)
+            # 优化器
+            self.optim = tf.train.AdamOptimizer(learning_rate=self.config.learning_rate).minimize(self.loss)
+
+        with tf.name_scope("accuracy"):
+            # 准确率
+            correct_pred = tf.equal(tf.argmax(self.input_y, 1), self.y_pred_cls)
+            self.acc = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
